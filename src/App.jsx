4d2c67ee68b93f8b12ed73e23626587b500d5152
GersonFlores03 <xdhaber12@gmail.com>
2024-01-24 22:36:01 -0500

    forms

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+         <h1 className='bg-red-400 flex justify-center'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
