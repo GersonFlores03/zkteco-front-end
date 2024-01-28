95514c5da1230d5d28f7d247da52497fca9151fc
Miguel1488 <maespinozav@gmail.com>
2024-01-27 23:44:53 -0500

    refresh

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,57 +3,49 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            <div>
-                <button className='aline items-center'>Registrate</button>
 
-            </div>
-            <input type="checkbox" name="hola" id="" />
-            <p className='sm:text-center lg:flex justify-start'>Quiero recibir oferta especiales, recomendaciones perzonalizadas y consejos de aprendizaje</p>
-            <div>
-                <button className="bg-pink-600 text-white font-bold py-2 px-20">
-                    Registrate
-                </button>
-            </div>
-            <div>
-                <p className="md:w-2/3 block text-gray-500">Al registarte, aceptas nuestras Condiciones de uso  y nuestras Política de privacidad </p>
-                <p>
-                    ¿Ya tienes una cuenta?
-                    <a href="#" style={{ textDecoration: 'underline', color: 'blue' }}>
-                        Inicia sesión
-                    </a>
+            <div className='restoreContainer sm:text-center'>
+                <h2 className='text sm:text-center text-2xl mb-5 lg:text-center font-bold'>REGISTRATE Y COMIENZA </h2>
 
-                </p>
-
-            </div>
-
-            <div className='flex flex-col h-screen w-full '>
-               
-                <div className='bg-gray-200 flex-grow'>
-
-
-
-
-                    <div className='p-20'>
-                        <button className="bg-blue-500 text-black  py-2 px-10 ">
-                            iniciar sesión
-                        </button>
-
+                <input type="text" placeholder='nombre completo' className='restoreInput sm: w-80 h-10 mb-5' />
+                <div className='ButtonRestore '>
+                    <div>
+                        <input type="text" placeholder='correo electrónico' className='restoreInput sm: w-80 h-10 mb-5' />
                     </div>
-                    <div className='py -20 px-10'>
-                        <button className="bg-black text-white  py-2 px-10 ">
-                            Registro
-                        </button>
+                    <div>
+                        <input type="text" placeholder='contraseña' className='restoreInput sm: w-80 h-10 mb-5' />
                     </div>
 
-                    <div className="  flex relative">
-                        <h2 className=" top-10 text-2xl font-bold tracking-tight text-blue-900">REGISTRATE Y COMIENZA</h2>
-
+                    <div>
+                        <input type="checkbox" name="hola" id="" />
+                        <p className='sm: w-70 h-5 mb-3'>Quiero recibir oferta especiales, recomendaciones perzonalizadas y consejos de aprendizaje</p>
                     </div>
 
+
+
+
+                    <button className=' buttonRestore sm: w-60 h-10 mb-3'> Registrate </button>
+                    <div>
+                        <p className="md:w-2/3 block text-gray-500">Al registarte, aceptas nuestras Condiciones de uso  y nuestras Política de privacidad </p>
+                        <p>
+                            ¿Ya tienes una cuenta?
+                            <a href="#" style={{ textDecoration: 'underline', color: 'blue' }}>
+                                Inicia sesión
+                            </a>
+
+                        </p>
+                    </div>
                 </div>
-            
+
+
+
+
+
             </div>
+
+
         </div>
+
     );
 };
 
