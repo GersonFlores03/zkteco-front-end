a6b1de2f42706d35dbacbf673df9dd3bf1ad6c6e
Miguel1488 <maespinozav@gmail.com>
2024-01-29 10:31:19 -0500

    refresh 1

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -15,10 +15,11 @@ const Register = () => {
                     <div>
                         <input type="text" placeholder='contraseña' className='restoreInput sm: w-80 h-10 mb-5' />
                     </div>
+                    <div className='-ml-80 -mb-5'><input type="checkbox" name="hola" id="" /></div>
 
                     <div>
-                        <input type="checkbox" name="hola" id="" />
-                        <p className='sm: w-70 h-5 mb-3'>Quiero recibir oferta especiales, recomendaciones perzonalizadas y consejos de aprendizaje</p>
+
+                        <p className='md:w-2/4 mx-auto text-xs xl:pr-4 mb-4'>Quiero recibir oferta especiales, recomendaciones perzonalizadas y consejos de aprendizaje</p>
                     </div>
 
 
@@ -26,7 +27,7 @@ const Register = () => {
 
                     <button className=' buttonRestore sm: w-60 h-10 mb-3'> Registrate </button>
                     <div>
-                        <p className="md:w-2/3 block text-gray-500">Al registarte, aceptas nuestras Condiciones de uso  y nuestras Política de privacidad </p>
+                        <p className="md:w-2/3 block text-gray-500 mx-auto mb-3">Al registarte, aceptas nuestras <u>Condiciones de uso</u>  y nuestras <u>Política de privacidad</u> </p>
                         <p>
                             ¿Ya tienes una cuenta?
                             <a href="#" style={{ textDecoration: 'underline', color: 'blue' }}>
